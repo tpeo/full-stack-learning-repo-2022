a502fbab0a5edb60a4063d4459bd604367fb95f3
connyycai <conny.cai@utexas.edu>
2022-11-04 13:51:43 -0500

    Week 3 Updates

--- Week_4/demo/demo-app/src/App.js
+++ Week_4/demo/demo-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <Button>Text</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
